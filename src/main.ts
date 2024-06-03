f9922664773f882fecf4d2fe2b8c19b9e7545ebe
VuViet <vuhoangviet@cryptopie-labo.com>
2024-06-04 00:04:50 +0700

    create workspace

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Marvel')
+    .setDescription('Marvel Mock API')
+    .setVersion('1.0')
+    .addTag('marvel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
+}
+bootstrap();
+
+// https://preview.themeforest.net/item/pickbazar-react-graphql-ecommerce-template/full_screen_preview/25283305?_ga=2.60876076.1043078509.1717343459-210415933.1716868967
